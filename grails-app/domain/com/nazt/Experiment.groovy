385467baaa76bb17d8e6e5d97b9847b4410afa4d
Nat <nat.weerawan@gmail.com>
2009-12-04 10:09:07 +0700

    add toString to return only Experiment Name in Experiment Domain [Model]

--- grails-app/domain/com/nazt/Experiment.groovy
+++ grails-app/domain/com/nazt/Experiment.groovy
@@ -3,7 +3,13 @@ package com.nazt
 class Experiment {
 	String name = '[Experiment #]'
 	Integer total
+	Date dateCreated
+	Date lastUpdated	
     static constraints = {
 		name(unique:true)
     }
+	String toString()
+	{
+		name	
+	}
 }
