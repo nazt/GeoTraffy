403ff335c983e9c93ad25928e07e8bbb696f7392
Nat <nat.weerawan@gmail.com>
2009-12-05 18:47:34 +0700

    add unit Test file

---
+++ test/unit/com/nazt/ExperimentTests.groovy
@@ -0,0 +1,17 @@
+package com.nazt
+
+import grails.test.*
+
+class ExperimentTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
---
+++ test/unit/com/nazt/SolutionTests.groovy
@@ -0,0 +1,17 @@
+package com.nazt
+
+import grails.test.*
+
+class SolutionTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
